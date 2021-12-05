57bb18ca5bfd7fa39c82797c8e6e7d1942289c62
cainsusk <63693601+cainsusk@users.noreply.github.com>
2021-12-05 14:49:02 -0500

    CISC220A6WIP2
    contains copies of average_grades.c to attempt different methods

--- y2.1/CISC220/A6/average_grades0.c
+++ y2.1/CISC220/A6/average_grades0.c
@@ -32,53 +32,21 @@ int countLines(FILE *file) {
   return lines;
 }
 
-int countFields(char *s, char *types){
-  char type = 'n';
+int countFields(char *s){
   int count = 0;
   int inString = 0;
 
   if (!strrchr(s, '\n')){
     strcat(s, (char*) "\n");
   }
-
-  printf("\n");
   for (int i=0; s[i]; i++){
-    printf("%c", s[i]);
-    if (isalpha(s[i]) || s[i] == '\"') {
-      type = 's';
-    }
-
-    if (isdigit(s[i])) {
-      type = 'd';
-    }
-
     inString = checkInString(s[i], inString);
     if ((s[i] == ',' || s[i] == '\n' ) && inString == 0){
-      *types++ = type;
-      type = 'n';
       count++;
     }
   }
   return count;
 }
-
-char* translateTypes(char *types, char *ans){
-  int count;
-  char add[21];
-  for (count=0; types[count]; count++){
-    switch (types[count]) {
-      case 'n':
-      strcat(add, " ,");
-      case 'd':
-      strcat(add, " %d,");
-      case 's':
-      strcat(add, " \"%*[^\"]\", ");
-    }
-    strcat(ans, add);
-    memset(add, 0, sizeof(add));
-  }
-}
-
 
 int main() {
   char first[21], last[21];
@@ -100,18 +68,8 @@ int main() {
   char line[100];
   fgets(line, 100, file);
 
-  rewind(file);
-  char buf[100];
-  char *types = (char*) (malloc(50*sizeof(char)));
-  char *translation = (char*) (malloc(50*sizeof(char)));
-    while (fgets(buf, 100, file)) {
-      int fields = countFields(buf, types);
-      translateTypes(types, translation);
-      printf("FIELDS: %d\n", fields);
-      printf("TYPES: %s\n", types);
-      printf("TYPES: %s\n", translation);
+  int fields = countFields(line);
+  printf("FIELDS: %d\n", fields);
 
 
-      types = (char*) (realloc(0, 50*sizeof(char)));
-    }
   }
